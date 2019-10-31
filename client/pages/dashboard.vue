be0b82fbf21b54cfc248420f17b6c973b8769192
Vitalii Kalaida <nevadskiy@gmail.com>
2019-10-31 21:45:27 +0200

    Set up base layout and navigation

---
+++ client/pages/dashboard.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="container mx-auto">
+    <h1>Secret dashboard for only authenticated users</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {
+  }
+}
+</script>
---
+++ client/pages/sign-in.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="container mx-auto">
+    signin into the app
+  </div>
+</template>
+
+<script>
+
+export default {
+
+}
+</script>
