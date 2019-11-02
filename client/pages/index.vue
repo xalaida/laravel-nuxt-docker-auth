163360c4361fc785ea6c9534974f87fdc1a9eb10
Vitalii Kalaida <nevadskiy@gmail.com>
2019-11-02 18:44:26 +0200

    Update index page

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,39 +1,15 @@
 <template>
-  <div class="container mx-auto">
-    <div>
-      <logo />
-      <h1 class="title">
-        client
-      </h1>
-      <h2 class="subtitle">
-        My well-made Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
+  <div class="justify-center items-center flex">
+    <AppLogo />
   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+import AppLogo from '~/components/AppLogo.vue'
 
 export default {
   components: {
-    Logo
+    AppLogo
   }
 }
 </script>
