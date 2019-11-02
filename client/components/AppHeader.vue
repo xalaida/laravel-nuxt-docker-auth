77b25fa7f5514319a2a8d36abf7700d8a1cf304b
Vitalii Kalaida <nevadskiy@gmail.com>
2019-11-02 14:51:47 +0200

    Add header component

---
+++ client/components/AppHeader.vue
@@ -0,0 +1,60 @@
+<template>
+  <header class="bg-teal-600">
+    <div class="container mx-auto">
+      <div class="flex py-3">
+        <nuxt-link
+          :to="{ name: 'index' }"
+          class="font-bold text-gray-100"
+        >
+          Home
+        </nuxt-link>
+
+        <nuxt-link
+          v-if="$auth.loggedIn"
+          :to="{ name: 'dashboard' }"
+          class="ml-12 font-bold text-gray-100"
+        >
+          Dashboard
+        </nuxt-link>
+
+        <div class="ml-auto">
+          <nuxt-link
+            v-if="!$auth.loggedIn"
+            :to="{ name: 'login' }"
+            class="font-bold text-gray-100"
+          >
+            Login
+          </nuxt-link>
+
+          <nuxt-link
+            v-if="!$auth.loggedIn"
+            :to="{ name: 'register' }"
+            class="ml-12 font-bold text-gray-100"
+          >
+            Register
+          </nuxt-link>
+
+          <button
+            v-if="$auth.loggedIn"
+            class="ml-12 font-bold text-gray-100"
+            @click.prevent="logout"
+          >
+            Logout
+          </button>
+        </div>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+import { mapActions } from 'vuex'
+
+export default {
+  methods: {
+    ...mapActions({
+      logout: 'auth/logout'
+    })
+  }
+}
+</script>
