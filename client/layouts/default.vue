371653141f82b1fc7a61ce026c9c1feaf2665b24
Vitalii Kalaida <nevadskiy@gmail.com>
2019-11-02 16:42:54 +0200

    Fix imports

--- client/layouts/default.vue
+++ client/layouts/default.vue
@@ -7,8 +7,8 @@
 </template>
 
 <script>
-import AppHeader from '../components/AppHeader.vue'
-import AppFooter from '../components/AppFooter.vue'
+import AppHeader from '~/components/AppHeader.vue'
+import AppFooter from '~/components/AppFooter.vue'
 
 export default {
   components: {
